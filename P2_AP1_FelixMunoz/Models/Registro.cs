7078f78b98cfd74cbfc38a77b0c2aedb977cb5d8
FelixAlbertoM <af301509@gmail.com>
2025-10-30 11:01:25 -0400

    registro

---
+++ P2_AP1_FelixMunoz/Models/Registro.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace P2_AP1_FelixMunoz.Models;
+
+public class Registro
+{
+    [Key]
+    public int IdRegistro { get; set; }
+
+    [Required(ErrorMessage = "Por favor digite la fecha.")]
+    public DateTime Fecha { get; set; } =DateTime.Now;
+
+}
